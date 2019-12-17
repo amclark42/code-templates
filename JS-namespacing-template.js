72fd135c452388463c7bd34868a4f34654a09e6f
Ashley M. Clark <amclark42@gmail.com>
2019-12-17 12:11:14 -0500

    Remove jQuery-specific test for the state of the DOM

--- JS-namespacing-template.js
+++ JS-namespacing-template.js
@@ -28,6 +28,18 @@ var ns = ns || {};
 }).apply(ns); // Apply the namespace to the anonymous function.
 
 
-$(document).ready( function() {
-  // Do something
-});
+// Create a callback function to be run when the entire document has loaded.
+var onLoad = function() {
+  console.log("Do something");
+};
+
+/* Ensure that the callback function above is run, whether or not the DOM has 
+  already been loaded. Solution by Julian Kühnel: 
+  https://www.sitepoint.com/jquery-document-ready-plain-javascript/ */
+if ( document.readyState === 'complete' 
+   || ( document.readyState !== 'loading' && !document.documentElement.doScroll ) 
+   ) {
+  onLoad();
+} else {
+  document.addEventListener('DOMContentLoaded', onLoad);
+}
